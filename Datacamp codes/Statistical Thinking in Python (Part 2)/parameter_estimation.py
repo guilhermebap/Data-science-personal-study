162a4e39c08c6835359184d76d24121a21f9208c
Guilherme Barros <guilhermebarrosap@gmail.com>
2019-10-26 23:42:22 -0300

    Sending new codes from datacamp

---
+++ Datacamp codes/Statistical Thinking in Python (Part 2)/parameter_estimation.py
@@ -0,0 +1,92 @@
+'''
+Code using hacker statistics to infer the best model and parameters
+'''
+
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+def ecdf(data):
+    """Compute ECDF for a one-dimensional array of measurements."""
+    # Number of data points: n
+    n = len(data)
+
+    # x-data for the ECDF: x
+    x = np.sort(data)
+
+    # y-data for the ECDF: y
+    y = np.arange(1, n+1) / n
+
+    return x, y
+
+'''
+Ploting a PDF from the data
+'''
+# Seed random number generator
+np.random.seed(42)
+
+# Compute mean no-hitter time: tau
+tau = np.mean(nohitter_times)
+
+# Draw out of an exponential distribution with parameter tau: inter_nohitter_time
+inter_nohitter_time = np.random.exponential(tau, 100000)
+
+# Plot the PDF and label axes
+_ = plt.hist(inter_nohitter_time,
+             bins=50, histtype='step', normed=True)
+_ = plt.xlabel('Games between no-hitters')
+_ = plt.ylabel('PDF')
+
+# Show the plot
+plt.show()
+
+'''
+Plotting a ECDF from the datas and comparing with theorical and empirical datas
+'''
+
+# Create an ECDF from real data: x, y
+x, y = ecdf(nohitter_times)
+
+# Create a CDF from theoretical samples: x_theor, y_theor
+x_theor, y_theor = ecdf(inter_nohitter_time)
+
+# Overlay the plots
+plt.plot(x_theor, y_theor)
+plt.plot(x, y, marker='.', linestyle=None)
+
+# Margins and axis labels
+plt.margins(0.02)
+plt.xlabel('Games between no-hitters')
+plt.ylabel('CDF')
+
+# Show the plot
+plt.show()
+
+'''
+Generating different exponential distribution number with different means
+so comparing how the parameter choosed was a good one
+'''
+# Plot the theoretical CDFs
+plt.plot(x_theor, y_theor)
+plt.plot(x, y, marker='.', linestyle='none')
+plt.margins(0.02)
+plt.xlabel('Games between no-hitters')
+plt.ylabel('CDF')
+
+# Take samples with half tau: samples_half
+samples_half = np.random.exponential(tau/2, size=10000)
+
+# Take samples with double tau: samples_double
+samples_double = np.random.exponential(tau*2, size=10000)
+
+# Generate CDFs from these samples
+x_half, y_half = ecdf(samples_half)
+x_double, y_double = ecdf(samples_double)
+
+# Plot these CDFs as lines
+_ = plt.plot(x_half, y_half)
+_ = plt.plot(x_double, y_double)
+
+# Show the plot
+plt.show()
