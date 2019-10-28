e67f535962f69e2ceceb9bc816b0d4fec66fcf39
Guilherme Barros <guilhermebarrosap@gmail.com>
2019-10-27 23:54:36 -0300

    Adding new exercises

--- Datacamp codes/Statistical Thinking in Python (Part 2)/parameter_estimation.py
+++ Datacamp codes/Statistical Thinking in Python (Part 2)/parameter_estimation.py
@@ -41,32 +41,16 @@ _ = plt.ylabel('PDF')
 # Show the plot
 plt.show()
 
-'''
-Plotting a ECDF from the datas and comparing with theorical and empirical datas
-'''
-
-# Create an ECDF from real data: x, y
-x, y = ecdf(nohitter_times)
-
-# Create a CDF from theoretical samples: x_theor, y_theor
-x_theor, y_theor = ecdf(inter_nohitter_time)
-
-# Overlay the plots
-plt.plot(x_theor, y_theor)
-plt.plot(x, y, marker='.', linestyle=None)
-
-# Margins and axis labels
-plt.margins(0.02)
-plt.xlabel('Games between no-hitters')
-plt.ylabel('CDF')
-
-# Show the plot
-plt.show()
 
 '''
 Generating different exponential distribution number with different means
 so comparing how the parameter choosed was a good one
 '''
+# Create an ECDF from real data: x, y
+x, y = ecdf(nohitter_times)
+
+# Create a CDF from theoretical samples: x_theor, y_theor
+x_theor, y_theor = ecdf(inter_nohitter_time)
 # Plot the theoretical CDFs
 plt.plot(x_theor, y_theor)
 plt.plot(x, y, marker='.', linestyle='none')
@@ -89,4 +73,61 @@ _ = plt.plot(x_half, y_half)
 _ = plt.plot(x_double, y_double)
 
 # Show the plot
+plt.show()
+
+
+def pearson_r(x, y):
+    """Compute Pearson correlation coefficient between two arrays."""
+    # Compute correlation matrix: corr_mat
+    corr_mat = np.corrcoef(x, y)
+
+
+    # Return entry [0,1]
+    return corr_mat[0,1]
+
+
+# Plot the illiteracy rate versus fertility
+_ = plt.plot(illiteracy, fertility, marker='.', linestyle='none')
+
+# Set the margins and label axes
+plt.margins(0.02)
+_ = plt.xlabel('percent illiterate')
+_ = plt.ylabel('fertility')
+
+# Show the Pearson correlation coefficient
+print(pearson_r(illiteracy, fertility))
+
+# Perform a linear regression using np.polyfit(): a, b
+a, b = np.polyfit(illiteracy, fertility, 1)
+
+# Print the results to the screen
+print('slope =', a, 'children per woman / percent illiterate')
+print('intercept =', b, 'children per woman')
+
+# Make theoretical line to plot
+x = np.array([0,100])
+y = a * x + b
+
+# Add regression line to your plot
+_ = plt.plot(x, y)
+
+# Draw the plot
+plt.show()
+
+
+# Specify slopes to consider: a_vals
+a_vals = np.linspace(0,0.1,200)
+
+# Initialize sum of square of residuals: rss
+rss = np.empty_like(a_vals)
+
+# Compute sum of square of residuals for each value of a_vals
+for i, a in enumerate(a_vals):
+    rss[i] = np.sum((fertility - a*illiteracy - b)**2)
+
+# Plot the RSS
+plt.plot(a_vals, rss, '-')
+plt.xlabel('slope (children per woman / percent illiterate)')
+plt.ylabel('sum of square of residuals')
+
 plt.show()
