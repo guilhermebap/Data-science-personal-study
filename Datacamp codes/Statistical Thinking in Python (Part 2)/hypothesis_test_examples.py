04f60408385e0c3dbd492a735a62b73920168727
Guilherme Barros <guilhermebarrosap@gmail.com>
2019-11-04 23:46:49 -0300

    Uploading new example from test hypothesis

--- Datacamp codes/Statistical Thinking in Python (Part 2)/hypothesis_test_examples.py
+++ Datacamp codes/Statistical Thinking in Python (Part 2)/hypothesis_test_examples.py
@@ -91,3 +91,22 @@ def diff_of_means(data_1, data_2):
     diff = (np.mean(data_1) - np.mean(data_2))
 
     return diff
+
+'''
+The vote for the Civil Rights Act in 1964
+'''
+# Construct arrays of data: dems, reps
+dems = np.array([True] * 153 + [False] * 91)
+reps = np.array([True] * 136 + [False] * 35)
+
+def frac_yea_dems(dems, reps):
+    """Compute fraction of Democrat yea votes."""
+    frac = np.sum(dems) / len(dems)
+    return frac
+
+# Acquire permutation samples: perm_replicates
+perm_replicates = draw_perm_reps(dems, reps, frac_yea_dems, size=10000)
+
+# Compute and print p-value: p
+p = np.sum(perm_replicates <= 153/244) / len(perm_replicates)
+print('p-value =', p)
