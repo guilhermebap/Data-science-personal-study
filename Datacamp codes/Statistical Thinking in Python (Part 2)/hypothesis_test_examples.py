b73cda4fc07552ac1efed4585d1b59bae14bc4a2
Guilherme Barros <guilhermebarrosap@gmail.com>
2019-11-05 20:21:28 -0300

    Uploading the last exercise from module 4 of statistics course

--- Datacamp codes/Statistical Thinking in Python (Part 2)/hypothesis_test_examples.py
+++ Datacamp codes/Statistical Thinking in Python (Part 2)/hypothesis_test_examples.py
@@ -110,3 +110,106 @@ perm_replicates = draw_perm_reps(dems, reps, frac_yea_dems, size=10000)
 # Compute and print p-value: p
 p = np.sum(perm_replicates <= 153/244) / len(perm_replicates)
 print('p-value =', p)
+
+'''
+A time-on-website analog
+
+In this problem you will perform an A/B test to determine if these rule changes
+resulted in a slower rate of no-hitters (i.e., longer average time between no-hitters)
+using the difference in mean inter-no-hitter time as your test statistic.
+The inter-no-hitter times for the respective eras are stored in the arrays nht_dead
+and nht_live, where "nht" is meant to stand for "no-hitter time."
+'''
+# Compute the observed difference in mean inter-no-hitter times: nht_diff_obs
+nht_diff_obs = diff_of_means(nht_dead, nht_live)
+
+# Acquire 10,000 permutation replicates of difference in mean no-hitter time: perm_replicates
+perm_replicates = draw_perm_reps(nht_dead, nht_live, diff_of_means, size=10000)
+
+# Compute and print the p-value: p
+p = np.sum(perm_replicates <= nht_diff_obs) / len(perm_replicates)
+print('p-val =', p)
+
+'''
+Hypothesis test on Pearson correlation
+
+The observed correlation between female illiteracy and fertility may just be by
+chance; the fertility of a given country may actually be totally independent of
+its illiteracy. You will test this hypothesis. To do so, permute the illiteracy
+values but leave the fertility values fixed.
+'''
+
+# Compute observed correlation: r_obs
+r_obs = pearson_r(illiteracy, fertility)
+
+# Initialize permutation replicates: perm_replicates
+perm_replicates = np.empty(10000)
+
+# Draw replicates
+for i in range(10000):
+    # Permute illiteracy measurments: illiteracy_permuted
+    illiteracy_permuted = np.random.permutation(illiteracy)
+
+    # Compute Pearson correlation
+    perm_replicates[i] = pearson_r(illiteracy_permuted, fertility)
+
+# Compute p-value: p
+p = np.sum(perm_replicates >= r_obs) / len(perm_replicates)
+print('p-val =', p)
+
+'''
+Do neonicotinoid insecticides have unintended consequences?
+
+First, we will do EDA
+'''
+# Compute x,y values for ECDFs
+x_control, y_control = ecdf(control)
+x_treated, y_treated = ecdf(treated)
+
+# Plot the ECDFs
+_ = plt.plot(x_control, y_control, marker='.', linestyle='none')
+_ = plt.plot(x_treated, y_treated, marker='.', linestyle='none')
+
+# Set the margins
+plt.margins(0.02)
+
+# Add a legend
+plt.legend(('control', 'treated'), loc='lower right')
+
+# Label axes and show plot
+plt.xlabel('millions of alive sperm per mL')
+plt.ylabel('ECDF')
+plt.show()
+
+'''
+Bootstrap hypothesis test on bee sperm counts
+
+Now, you will test the following hypothesis: On average, male bees treated
+neonicotinoid insecticide have the same number of active sperm per milliliter
+of semen than do untreated male bees. You will use the difference of means as
+your test statistic.
+'''
+
+# Compute the difference in mean sperm count: diff_means
+diff_means = np.mean(control) - np.mean(treated)
+
+# Compute mean of pooled data: mean_count
+mean_count = np.mean(np.concatenate((control, treated)))
+
+# Generate shifted data sets
+control_shifted = control - np.mean(control) + mean_count
+treated_shifted = treated - np.mean(treated) + mean_count
+
+# Generate bootstrap replicates
+bs_reps_control = draw_bs_reps(control_shifted,
+                       np.mean, size=10000)
+bs_reps_treated = draw_bs_reps(treated_shifted,
+                       np.mean, size=10000)
+
+# Get replicates of difference of means: bs_replicates
+bs_replicates = bs_reps_control - bs_reps_treated
+
+# Compute and print p-value: p
+p = np.sum(bs_replicates >= np.mean(control) - np.mean(treated)) \
+            / len(bs_replicates)
+print('p-value =', p)
