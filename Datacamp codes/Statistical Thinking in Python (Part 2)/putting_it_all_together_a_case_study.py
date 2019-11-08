13d6a863dbaa398f1804b90680f0fd5edf1bc745
Guilherme Barros <guilhermebarrosap@gmail.com>
2019-11-07 23:57:30 -0300

    Uploading more exercises

--- Datacamp codes/Statistical Thinking in Python (Part 2)/putting_it_all_together_a_case_study.py
+++ Datacamp codes/Statistical Thinking in Python (Part 2)/putting_it_all_together_a_case_study.py
@@ -91,3 +91,62 @@ def diff_of_means(data_1, data_2):
     diff = (np.mean(data_1) - np.mean(data_2))
 
     return diff
+
+'''
+EDA of beak depths of Darwin's finches
+'''
+# Create bee swarm plot
+_ = sns.swarmplot(x='year', y='beak_depth', data=df)
+
+# Label the axes
+_ = plt.xlabel('year')
+_ = plt.ylabel('beak depth (mm)')
+
+# Show the plot
+plt.show()
+
+'''
+ECDFs of beak depths
+'''
+# Compute ECDFs
+x_1975, y_1975 = ecdf(bd_1975)
+x_2012, y_2012 = ecdf(bd_2012)
+
+# Plot the ECDFs
+_ = plt.plot(x_1975, y_1975, marker='.', linestyle='none')
+_ = plt.plot(x_2012, y_2012, marker='.', linestyle='none')
+
+# Set margins
+plt.margins(0.02)
+
+# Add axis labels and legend
+_ = plt.xlabel('beak depth (mm)')
+_ = plt.ylabel('ECDF')
+_ = plt.legend(('1975', '2012'), loc='lower right')
+
+# Show the plot
+plt.show()
+
+'''
+Parameter estimates of beak depths
+'''
+# Compute the difference of the sample means: mean_diff
+mean_diff = np.mean(bd_2012) - np.mean(bd_1975)
+
+# Get bootstrap replicates of means
+bs_replicates_1975 = draw_bs_reps(bd_1975, np.mean, size=10000)
+bs_replicates_2012 = draw_bs_reps(bd_2012, np.mean, size=10000)
+
+# Compute samples of difference of means: bs_diff_replicates
+bs_diff_replicates = bs_replicates_2012 - bs_replicates_1975
+
+# Compute 95% confidence interval: conf_int
+conf_int = np.percentile(bs_diff_replicates, [2.5, 97.5])
+
+# Print the results
+print('difference of means =', mean_diff, 'mm')
+print('95% confidence interval =', conf_int, 'mm')
+
+'''
+Hypothesis test: Are beaks deeper in 2012?
+'''
