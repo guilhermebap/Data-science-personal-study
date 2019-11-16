3a6ef492460f92daf6f0a1b0f20ce12da1ffc23e
Guilhermebap <guilhermebarrosap@gmail.com>
2019-11-15 23:53:22 -0300

    Uploading more exercises

---
+++ Datacamp codes/Interactive Data Visualization with Bokeh/Layouts_Interactions_and_Annotations.py
@@ -0,0 +1,54 @@
+import matplotlib.pyplot as plot
+import pandas as pd
+import numpy as np
+
+'''
+Creating rows of plots
+'''
+# Import row from bokeh.layouts
+from bokeh.layouts import row
+
+# Create the first figure: p1
+p1 = figure(x_axis_label='fertility (children per woman)', y_axis_label='female_literacy (% population)')
+
+# Add a circle glyph to p1
+p1.circle('fertility', 'female_literacy', source=source)
+
+# Create the second figure: p2
+p2 = figure(x_axis_label='population', y_axis_label='female_literacy (% population)')
+
+# Add a circle glyph to p2
+p2.circle('population', 'female_literacy', source=source)
+
+# Put p1 and p2 into a horizontal row: layout
+layout = row(p1, p2)
+
+# Specify the name of the output_file and show the result
+output_file('fert_row.html')
+show(layout)
+
+
+'''
+Creating columns of plots
+'''
+# Import column from the bokeh.layouts module
+from bokeh.layouts import column
+
+# Create a blank figure: p1
+p1 = figure(x_axis_label='fertility (children per woman)', y_axis_label='female_literacy (% population)')
+
+# Add circle scatter to the figure p1
+p1.circle('fertility', 'female_literacy', source=source)
+
+# Create a new blank figure: p2
+p2 = figure(x_axis_label='population', y_axis_label='female_literacy (% population)')
+
+# Add circle scatter to the figure p2
+p2.circle('population', 'female_literacy', source=source)
+
+# Put plots p1 and p2 in a column: layout
+layout = column(p1, p2)
+
+# Specify the name of the output_file and show the result
+output_file('fert_column.html')
+show(layout)
