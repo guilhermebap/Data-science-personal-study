0d3c9619101f3f88f51d6e645b9962858255a791
Guilhermebap <guilhermebarrosap@gmail.com>
2019-11-17 18:03:17 -0300

    Uploading more exercises

--- Datacamp codes/Interactive Data Visualization with Bokeh/building_interactive_apps_with_Bokeh.py
+++ Datacamp codes/Interactive Data Visualization with Bokeh/building_interactive_apps_with_Bokeh.py
@@ -5,3 +5,51 @@ import numpy as np
 '''
 Using the current document
 '''
+# Perform necessary imports
+from bokeh.io import curdoc
+from bokeh.plotting import figure
+
+# Create a new plot: plot
+plot = figure()
+
+# Add a line to the plot
+plot.line([1,2,3,4,5], [2,5,4,6,7])
+# Add the plot to the current document
+curdoc().add_root(plot)
+
+
+'''
+Add a single slider
+'''
+# Perform the necessary imports
+from bokeh.io import curdoc
+from bokeh.layouts import widgetbox
+from bokeh.models import Slider
+
+# Create a slider: slider
+slider = Slider(title='my slider', start=0, end=10, step=0.1, value=2)
+
+# Create a widgetbox layout: layout
+layout = widgetbox(slider)
+
+# Add the layout to the current document
+curdoc().add_root(layout)
+
+
+'''
+Multiple sliders in one document
+'''
+# Create first slider: slider1
+slider1 = Slider(title='slider1', start=0, end=10, step=0.1, value=2)
+
+# Create second slider: slider2
+slider2 = Slider(title='slider2', start=10, end=100, step=1, value=20)
+
+# Add slider1 and slider2 to a widgetbox
+layout = widgetbox(slider1, slider2)
+
+# Add the layout to the current document
+curdoc().add_root(layout)
+
+
+'''
