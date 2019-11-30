4433466124be4a1efdc5aaed60eda0f89f09054e
Guilhermebap <guilhermebarrosap@gmail.com>
2019-11-29 23:59:25 -0300

    Uploading more exercises did

--- Datacamp codes/Supervised Learning with scikit-learn/regression.py
+++ Datacamp codes/Supervised Learning with scikit-learn/regression.py
@@ -2,6 +2,9 @@ import pandas as pd
 import matplotlib as plt
 import numpy as np
 from sklearn import datasets
+import seaborn as sns
+
+df = pd.read_csv('../gapminder.csv')
 
 '''
 Importing data for supervised learning
@@ -9,4 +12,55 @@ Importing data for supervised learning
 Since the target variable here is quantitative, this is a regression problem.
 To begin, you will fit a linear regression with just one feature: 'fertility'
 '''
-df = pd.read_csv('../gapminder.csv')
+
+# Read the CSV file into a DataFrame: df
+df = pd.read_csv('gapminder.csv')
+
+# Create arrays for features and target variable
+y = df['life'].values
+X = df['fertility'].values
+
+# Print the dimensions of X and y before reshaping
+print("Dimensions of y before reshaping: {}".format(y.shape))
+print("Dimensions of X before reshaping: {}".format(X.shape))
+
+# Reshape X and y
+y = y.reshape(-1, 1)
+X = X.reshape(-1, 1)
+
+# Print the dimensions of X and y after reshaping
+print("Dimensions of y after reshaping: {}".format(y.shape))
+print("Dimensions of X after reshaping: {}".format(X.shape))
+
+
+'''
+Exploring the Gapminder data
+'''
+sns.heatmap(df.corr(), square=True, cmap='RdYlGn')
+plt.show()
+
+
+'''
+Fit & predict for regression
+'''
+# Import LinearRegression
+from sklearn.linear_model import LinearRegression
+
+# Create the regressor: reg
+reg = LinearRegression()
+
+# Create the prediction space
+prediction_space = np.linspace(min(X_fertility), max(X_fertility)).reshape(-1,1)
+
+# Fit the model to the data
+reg.fit(X_fertility, y)
+
+# Compute predictions over the prediction space: y_pred
+y_pred = reg.predict(prediction_space)
+
+# Print R^2
+print(reg.score(X_fertility, y))
+
+# Plot regression line
+plt.plot(prediction_space, y_pred, color='black', linewidth=3)
+plt.show()
