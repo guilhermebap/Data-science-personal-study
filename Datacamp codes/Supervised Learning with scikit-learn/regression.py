a89c34ed5ceebf67561cc671badcebd5f1ffc8c8
Guilhermebap <guilhermebarrosap@gmail.com>
2019-11-30 23:21:28 -0300

    Uploading more exercises did

--- Datacamp codes/Supervised Learning with scikit-learn/regression.py
+++ Datacamp codes/Supervised Learning with scikit-learn/regression.py
@@ -4,6 +4,7 @@ import numpy as np
 from sklearn import datasets
 import seaborn as sns
 
+
 df = pd.read_csv('../gapminder.csv')
 
 '''
@@ -64,3 +65,74 @@ print(reg.score(X_fertility, y))
 # Plot regression line
 plt.plot(prediction_space, y_pred, color='black', linewidth=3)
 plt.show()
+
+
+'''
+Train/test split for regression
+'''
+# Import necessary modules
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+
+# Create training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state=42)
+
+# Create the regressor: reg_all
+reg_all = LinearRegression()
+
+# Fit the regressor to the training data
+reg_all.fit(X_train, y_train)
+
+# Predict on the test data: y_pred
+y_pred = reg_all.predict(X_test)
+
+# Compute and print R^2 and RMSE
+print("R^2: {}".format(reg_all.score(X_test, y_test)))
+rmse = np.sqrt(mean_squared_error(y_test, y_pred))
+print("Root Mean Squared Error: {}".format(rmse))
+
+# R^2: 0.838046873142936
+# Root Mean Squared Error: 3.2476010800377213
+
+'''
+5-fold cross-validation
+'''
+from sklearn.model_selection import cross_val_score
+
+# Create a linear regression object: reg
+reg = LinearRegression()
+
+# Compute 5-fold cross-validation scores: cv_scores
+cv_scores = cross_val_score(reg, X, y, cv=5)
+
+# Print the 5-fold cross-validation scores
+print(cv_scores)
+
+print("Average 5-Fold CV Score: {}".format(np.mean(cv_scores)))
+
+
+'''
+K-Fold CV comparison
+
+In the IPython Shell, you can use %timeit to see how long each 3-fold CV
+takes compared to 10-fold CV by executing the following cv=3 and cv=10
+
+%timeit cross_val_score(reg, X, y, cv = ____)
+'''
+# Import necessary modules
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import cross_val_score
+
+# Create a linear regression object: reg
+reg = LinearRegression()
+
+# Perform 3-fold CV
+cvscores_3 = cross_val_score(reg, X, y, cv=3)
+print(np.mean(cvscores_3))
+
+# Perform 10-fold CV
+cvscores_10 = cross_val_score(reg, X, y, cv=10)
+print(np.mean(cvscores_10))
+
+
+'''
