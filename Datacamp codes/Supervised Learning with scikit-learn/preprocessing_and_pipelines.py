34f1055d0810eee1e517d58419952584f1c896d2
Guilhermebap <guilhermebarrosap@gmail.com>
2019-12-05 11:41:36 -0300

    Uploading more exercises did

---
+++ Datacamp codes/Supervised Learning with scikit-learn/preprocessing_and_pipelines.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import matplotlib as plt
+import numpy as np
+from sklearn import datasets
+import seaborn as sns
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
