f81a9278bdf15bf75dbd97aff8e42467fd5dd583
Guilhermebap <guilhermebarrosap@gmail.com>
2019-12-06 02:02:43 -0300

    Uploading more exercises did

--- Datacamp codes/Supervised Learning with scikit-learn/preprocessing_and_pipelines.py
+++ Datacamp codes/Supervised Learning with scikit-learn/preprocessing_and_pipelines.py
@@ -5,3 +5,50 @@ from sklearn import datasets
 import seaborn as sns
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
+
+'''
+Exploring categorical features
+'''
+# Read 'gapminder.csv' into a DataFrame: df
+df = pd.read_csv('gapminder.csv')
+
+# Create a boxplot of life expectancy per region
+df.boxplot('life', 'Region', rot=60)
+
+# Show the plot
+plt.show()
+
+
+'''
+Creating dummy variables
+'''
+# Create dummy variables: df_region
+df_region = pd.get_dummies(df)
+
+# Print the columns of df_region
+print(df_region.columns)
+
+# Create dummy variables with drop_first=True: df_region
+df_region = pd.get_dummies(df, drop_first=True)
+
+# Print the new columns of df_region
+print(df_region.columns)
+
+
+'''
+Regression with categorical features
+'''
+# Import necessary modules
+from sklearn.model_selection import cross_val_score
+from sklearn.linear_model import Ridge
+
+# Instantiate a ridge regressor: ridge
+ridge = Ridge(alpha=0.5, normalize=True)
+
+# Perform 5-fold cross-validation: ridge_cv
+ridge_cv = cross_val_score(ridge, X, y, cv=5)
+
+# Print the cross-validated scores
+print(ridge_cv)
+
+'''
