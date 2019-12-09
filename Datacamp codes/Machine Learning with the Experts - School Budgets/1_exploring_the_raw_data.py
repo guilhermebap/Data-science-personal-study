9be65ac8000ec15bdf71cfa084f1a839889fa035
Guilhermebap <guilhermebarrosap@gmail.com>
2019-12-09 01:14:39 -0300

    Uploading more exercises did

--- Datacamp codes/Machine Learning with the Experts - School Budgets/1_exploring_the_raw_data.py
+++ Datacamp codes/Machine Learning with the Experts - School Budgets/1_exploring_the_raw_data.py
@@ -35,3 +35,47 @@ print(df[LABELS].dtypes)
 
 
 '''
+Counting unique labels
+'''
+# Import matplotlib.pyplot
+import matplotlib.pyplot as plt
+
+# Calculate number of unique values for each label: num_unique_labels
+num_unique_labels = df[LABELS].apply(pd.Series.nunique)
+
+# Plot number of unique values for each label
+num_unique_labels.plot(kind='bar')
+
+# Label the axes
+plt.xlabel('Labels')
+plt.ylabel('Number of unique values')
+
+# Display the plot
+plt.show()
+
+
+'''
+Computing log loss with NumPy
+'''
+# Compute and print log loss for 1st case
+correct_confident_loss = compute_log_loss(correct_confident, actual_labels)
+print("Log loss, correct and confident: {}".format(correct_confident_loss))
+
+# Compute log loss for 2nd case
+correct_not_confident_loss = compute_log_loss(correct_not_confident, actual_labels)
+print("Log loss, correct and not confident: {}".format(correct_not_confident_loss))
+
+# Compute and print log loss for 3rd case
+wrong_not_confident_loss = compute_log_loss(wrong_not_confident, actual_labels)
+print("Log loss, wrong and not confident: {}".format(wrong_not_confident_loss))
+
+# Compute and print log loss for 4th case
+wrong_confident_loss = compute_log_loss(wrong_confident, actual_labels)
+print("Log loss, wrong and confident: {}".format(wrong_confident_loss))
+
+# Compute and print log loss for actual labels
+actual_labels_loss = compute_log_loss(actual_labels, actual_labels)
+print("Log loss, actual labels: {}".format(actual_labels_loss))
+
+
+'''
