9dd357fd6ef34c967e0426175ac1ec9e260ab7d1
Guilhermebap <guilhermebarrosap@gmail.com>
2019-12-11 23:50:26 -0300

    Uploading more exercises did

---
+++ Datacamp codes/Machine Learning with the Experts - School Budgets/2_creating_a_simple_first_model.py
@@ -0,0 +1,58 @@
+import pandas as pd
+
+'''
+Setting up a train-test split in scikit-learn
+'''
+# Create the new DataFrame: numeric_data_only
+numeric_data_only = df[NUMERIC_COLUMNS].fillna(-1000)
+
+# Get labels and convert to dummy variables: label_dummies
+label_dummies = pd.get_dummies(df[LABELS])
+
+# Create training and test sets
+X_train, X_test, y_train, y_test = multilabel_train_test_split(numeric_data_only,
+                                                               label_dummies,
+                                                               size=0.2,
+                                                               seed=123)
+
+# Print the info
+print("X_train info:")
+print(X_train.info())
+print("\nX_test info:")
+print(X_test.info())
+print("\ny_train info:")
+print(y_train.info())
+print("\ny_test info:")
+print(y_test.info())
+
+
+'''
+Training a model
+'''
+# Import classifiers
+from sklearn.linear_model import LogisticRegression
+from sklearn.multiclass import OneVsRestClassifier
+
+# Create the DataFrame: numeric_data_only
+numeric_data_only = df[NUMERIC_COLUMNS].fillna(-1000)
+
+# Get labels and convert to dummy variables: label_dummies
+label_dummies = pd.get_dummies(df[LABELS])
+
+# Create training and test sets
+X_train, X_test, y_train, y_test = multilabel_train_test_split(numeric_data_only,
+                                                               label_dummies,
+                                                               size=0.2,
+                                                               seed=123)
+
+# Instantiate the classifier: clf
+clf = OneVsRestClassifier(LogisticRegression())
+
+# Fit the classifier to the training data
+clf.fit(X_train, y_train)
+
+# Print the accuracy
+print("Accuracy: {}".format(clf.score(X_test, y_test)))
+
+
+'''
