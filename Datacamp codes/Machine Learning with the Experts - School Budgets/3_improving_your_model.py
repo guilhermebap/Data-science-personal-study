bdd884c03880e65bbefeee0a6fdae70e747b162e
Guilhermebap <guilhermebarrosap@gmail.com>
2019-12-20 01:49:23 -0300

    Uploading more exercises did

--- Datacamp codes/Machine Learning with the Experts - School Budgets/3_improving_your_model.py
+++ Datacamp codes/Machine Learning with the Experts - School Budgets/3_improving_your_model.py
@@ -125,19 +125,139 @@ process_and_join_features = FeatureUnion(
                     ('selector', get_text_data),
                     ('vectorizer', CountVectorizer())
                 ]))
-             ]
-        )
-
+             ])
 # Instantiate nested pipeline: pl
 pl = Pipeline([
         ('union', process_and_join_features),
         ('clf', OneVsRestClassifier(LogisticRegression()))
-    ])
-
-
+        ])
 # Fit pl to the training data
 pl.fit(X_train, y_train)
 
 # Compute and print accuracy
 accuracy = pl.score(X_test, y_test)
 print("\nAccuracy on sample data - all data: ", accuracy)
+
+
+
+'''
+Using FunctionTransformer on the main dataset
+'''
+# Import FunctionTransformer
+from sklearn.preprocessing import FunctionTransformer
+
+# Get the dummy encoding of the labels
+dummy_labels = pd.get_dummies(df[LABELS])
+
+# Get the columns that are features in the original df
+NON_LABELS = [c for c in df.columns if c not in LABELS]
+
+# Split into training and test sets
+X_train, X_test, y_train, y_test = multilabel_train_test_split(df[NON_LABELS],
+                                                               dummy_labels,
+                                                               0.2,
+                                                               seed=123)
+# Preprocess the text data: get_text_data
+get_text_data = FunctionTransformer(combine_text_columns, validate=False)
+
+# Preprocess the numeric data: get_numeric_data
+get_numeric_data = FunctionTransformer(lambda x: x[NUMERIC_COLUMNS], validate=False)
+
+
+
+'''
+Add a model to the pipeline
+
+The structure of the pipeline:
+
+The preprocessing step uses FeatureUnion to join the results of nested pipelines
+that each rely on FunctionTransformer to select multiple datatypes
+the model step stores the model object
+'''
+# Complete the pipeline: pl
+pl = Pipeline([
+        ('union', FeatureUnion(
+            transformer_list = [
+                ('numeric_features', Pipeline([
+                    ('selector', get_numeric_data),
+                    ('imputer', Imputer())
+                ])),
+                ('text_features', Pipeline([
+                    ('selector', get_text_data),
+                    ('vectorizer', CountVectorizer())
+                ]))
+             ]
+        )),
+        ('clf', OneVsRestClassifier(LogisticRegression()))
+    ])
+
+# Fit to the training data
+pl.fit(X_train, y_train)
+
+# Compute and print accuracy
+accuracy = pl.score(X_test, y_test)
+print("\nAccuracy on budget dataset: ", accuracy)
+
+
+
+'''
+Try a different class of model
+'''
+# Import random forest classifer
+from sklearn.ensemble import RandomForestClassifier
+
+# Edit model step in pipeline
+pl = Pipeline([
+        ('union', FeatureUnion(
+            transformer_list = [
+                ('numeric_features', Pipeline([
+                    ('selector', get_numeric_data),
+                    ('imputer', Imputer())
+                ])),
+                ('text_features', Pipeline([
+                    ('selector', get_text_data),
+                    ('vectorizer', CountVectorizer())
+                ]))
+             ]
+        )),
+        ('clf', RandomForestClassifier())
+    ])
+
+# Fit to the training data
+pl.fit(X_train, y_train)
+
+# Compute and print accuracy
+accuracy = pl.score(X_test, y_test)
+print("\nAccuracy on budget dataset: ", accuracy)
+
+
+
+'''
+Can you adjust the model or parameters to improve accuracy?
+'''
+# Import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier
+
+# Add model step to pipeline: pl
+pl = Pipeline([
+        ('union', FeatureUnion(
+            transformer_list = [
+                ('numeric_features', Pipeline([
+                    ('selector', get_numeric_data),
+                    ('imputer', Imputer())
+                ])),
+                ('text_features', Pipeline([
+                    ('selector', get_text_data),
+                    ('vectorizer', CountVectorizer())
+                ]))
+             ]
+        )),
+        ('clf', RandomForestClassifier(n_estimators=15))
+    ])
+
+# Fit to the training data
+pl.fit(X_train, y_train)
+
+# Compute and print accuracy
+accuracy = pl.score(X_test, y_test)
+print("\nAccuracy on budget dataset: ", accuracy)
