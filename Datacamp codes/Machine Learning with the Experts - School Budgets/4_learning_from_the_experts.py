5b58f7a174fa6d158f14901be84eb0751b83b92f
Guilhermebap <guilhermebarrosap@gmail.com>
2019-12-22 20:54:19 -0300

    Uploading more exercises did

--- Datacamp codes/Machine Learning with the Experts - School Budgets/4_learning_from_the_experts.py
+++ Datacamp codes/Machine Learning with the Experts - School Budgets/4_learning_from_the_experts.py
@@ -1,1 +1,95 @@
 import pandas as pd
+
+
+'''
+Deciding what's a word
+'''
+# Import the CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
+
+# Create the text vector
+text_vector = combine_text_columns(X_train)
+
+# Create the token pattern: TOKENS_ALPHANUMERIC
+TOKENS_ALPHANUMERIC = '[A-Za-z0-9]+(?=\\s+)'
+
+# Instantiate the CountVectorizer: text_features
+text_features = CountVectorizer(token_pattern=TOKENS_ALPHANUMERIC)
+
+# Fit text_features to the text vector
+text_features.fit(text_vector)
+
+# Print the first 10 tokens
+print(text_features.get_feature_names()[:10])
+
+
+
+'''
+N-gram range in scikit-learn
+
+Special functions: You'll notice a couple of new steps provided in the pipeline
+in this and many of the remaining exercises. Specifically, the dim_red step
+following the vectorizer step , and the scale step preceeding the clf (classification) step.
+
+These have been added in order to account for the fact that you're using a
+reduced-size sample of the full dataset in this course. To make sure the models
+perform as the expert competition winner intended, we have to apply a dimensionality
+reduction technique, which is what the dim_red step does, and we have to scale the
+features to lie between -1 and 1, which is what the scale step does.
+
+The dim_red step uses a scikit-learn function called SelectKBest(), applying
+something called the chi-squared test to select the K "best" features. The scale
+step uses a scikit-learn function called MaxAbsScaler() in order to squash the
+relevant features into the interval -1 to 1.
+'''
+# Import pipeline
+from sklearn.pipeline import Pipeline
+
+# Import classifiers
+from sklearn.linear_model import LogisticRegression
+from sklearn.multiclass import OneVsRestClassifier
+
+# Import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
+
+# Import other preprocessing modules
+from sklearn.preprocessing import Imputer
+from sklearn.feature_selection import chi2, SelectKBest
+
+# Select 300 best features
+chi_k = 300
+
+# Import functional utilities
+from sklearn.preprocessing import FunctionTransformer, MaxAbsScaler
+from sklearn.pipeline import FeatureUnion
+
+# Perform preprocessing
+get_text_data = FunctionTransformer(combine_text_columns, validate=False)
+get_numeric_data = FunctionTransformer(lambda x: x[NUMERIC_COLUMNS], validate=False)
+
+# Create the token pattern: TOKENS_ALPHANUMERIC
+TOKENS_ALPHANUMERIC = '[A-Za-z0-9]+(?=\\s+)'
+
+# Instantiate pipeline: pl
+pl = Pipeline([
+        ('union', FeatureUnion(
+            transformer_list = [
+                ('numeric_features', Pipeline([
+                    ('selector', get_numeric_data),
+                    ('imputer', Imputer())
+                ])),
+                ('text_features', Pipeline([
+                    ('selector', get_text_data),
+                    ('vectorizer', CountVectorizer(token_pattern=TOKENS_ALPHANUMERIC,
+                                                   ngram_range=(1,2))),
+                    ('dim_red', SelectKBest(chi2, chi_k))
+                ]))
+             ]
+        )),
+        ('scale', MaxAbsScaler()),
+        ('clf', OneVsRestClassifier(LogisticRegression()))
+    ])
+
+
+
+'''
