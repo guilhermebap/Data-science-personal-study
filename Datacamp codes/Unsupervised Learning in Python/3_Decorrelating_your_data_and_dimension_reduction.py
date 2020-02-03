ee66bcc1d1a95f8ddac614e7a4ae021eb7b1558a
Guilhermebap <guilhermebarrosap@gmail.com>
2020-02-02 23:57:41 -0300

    Uploading more exercises did

--- Datacamp codes/Unsupervised Learning in Python/3_Decorrelating_your_data_and_dimension_reduction.py
+++ Datacamp codes/Unsupervised Learning in Python/3_Decorrelating_your_data_and_dimension_reduction.py
@@ -1,3 +1,12 @@
+'''
+Dimension reduction summarizes a dataset using its common occuring patterns.
+In this chapter, you'll learn about the most fundamental of dimension reduction
+techniques, "Principal Component Analysis" ("PCA"). PCA is often used before
+supervised learning to improve model performance and generalization. It can also
+be useful for unsupervised learning. For example, you'll employ a variant of PCA
+will allow you to cluster Wikipedia articles by their content!
+'''
+
 import pandas as pd
 import matplotlib.pyplot as plt
 
