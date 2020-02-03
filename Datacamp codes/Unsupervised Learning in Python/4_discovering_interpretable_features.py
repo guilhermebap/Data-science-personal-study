f7b95077cc1c68e04c5f0291a402879cf3af9a97
Guilhermebap <guilhermebarrosap@gmail.com>
2020-02-02 23:59:33 -0300

    Uploading more exercises did

---
+++ Datacamp codes/Unsupervised Learning in Python/4_discovering_interpretable_features.py
@@ -0,0 +1,43 @@
+'''
+In this chapter, you'll learn about a dimension reduction technique called
+"Non-negative matrix factorization" ("NMF") that expresses samples as combinations
+of interpretable parts. For example, it expresses documents as combinations of
+topics, and images in terms of commonly occurring visual patterns. You'll also
+learn to use NMF to build recommender systems that can find you similar articles
+to read, or musical artists that match your listening history!
+'''
+
+'''
+NMF applied to Wikipedia articles
+'''
+# Import NMF
+from sklearn.decomposition import NMF
+
+# Create an NMF instance: model
+model = NMF(n_components=6)
+
+# Fit the model to articles
+model.fit(articles)
+
+# Transform the articles: nmf_features
+nmf_features = model.transform(articles)
+
+# Print the NMF features
+print(nmf_features)
+
+
+
+'''
+NMF features of the Wikipedia articles
+'''
+# Import pandas
+import pandas as pd
+
+# Create a pandas DataFrame: df
+df =  pd.DataFrame(nmf_features, index=titles)
+
+# Print the row for 'Anne Hathaway'
+print(df.loc['Anne Hathaway'])
+
+# Print the row for 'Denzel Washington'
+print(df.loc['Denzel Washington'])
