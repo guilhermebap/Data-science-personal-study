414f184297e12861be27d44b4aa59fcaa74c1e4a
Guilhermebap <guilhermebarrosap@gmail.com>
2020-02-06 22:49:57 -0300

    Uploading more exercises did

--- Datacamp codes/Unsupervised Learning in Python/4_discovering_interpretable_features.py
+++ Datacamp codes/Unsupervised Learning in Python/4_discovering_interpretable_features.py
@@ -124,3 +124,96 @@ print(digit_features)
 '''
 PCA doesn't learn parts
 '''
+# Import PCA
+from sklearn.decomposition import PCA
+
+# Create a PCA instance: model
+model = PCA(n_components=7)
+
+# Apply fit_transform to samples: features
+features = model.fit_transform(samples)
+
+# Call show_as_image on each component
+for component in model.components_:
+    show_as_image(component)
+
+
+
+'''
+Which articles are similar to 'Cristiano Ronaldo'?
+'''
+# Perform the necessary imports
+import pandas as pd
+from sklearn.preprocessing import normalize
+
+# Normalize the NMF features: norm_features
+norm_features = normalize(nmf_features)
+
+# Create a DataFrame: df
+df = pd.DataFrame(norm_features, index=titles)
+
+# Select the row corresponding to 'Cristiano Ronaldo': article
+article = df.loc['Cristiano Ronaldo']
+
+# Compute the dot products: similarities
+similarities = df.dot(article)
+
+# Display those with the largest cosine similarity
+print(similarities.nlargest())
+
+
+
+'''
+Recommend musical artists part I
+
+In this exercise and the next, you'll use what you've learned about NMF to
+recommend popular music artists! You are given a sparse array artists whose rows
+correspond to artists and whose column correspond to users. The entries give the
+number of times each artist was listened to by each user.
+
+In this exercise, build a pipeline and transform the array into normalized NMF
+features. The first step in the pipeline, MaxAbsScaler, transforms the data so
+that all users have the same influence on the model, regardless of how many
+different artists they've listened to. In the next exercise, you'll use the
+resulting normalized NMF features for recommendation!
+'''
+# Perform the necessary imports
+from sklearn.decomposition import NMF
+from sklearn.preprocessing import Normalizer, MaxAbsScaler
+from sklearn.pipeline import make_pipeline
+
+# Create a MaxAbsScaler: scaler
+scaler = MaxAbsScaler()
+
+# Create an NMF model: nmf
+nmf = NMF(n_components=20)
+
+# Create a Normalizer: normalizer
+normalizer = Normalizer()
+
+# Create a pipeline: pipeline
+pipeline = make_pipeline(scaler,nmf,normalizer)
+
+# Apply fit_transform to artists: norm_features
+norm_features = pipeline.fit_transform(artists)
+
+
+
+
+'''
+Recommend musical artists part II
+'''
+# Import pandas
+import pandas as pd
+
+# Create a DataFrame: df
+df = pd.DataFrame(norm_features, index=artist_names)
+
+# Select row of 'Bruce Springsteen': artist
+artist = df.loc['Bruce Springsteen']
+
+# Compute cosine similarities: similarities
+similarities = df.dot(artist)
+
+# Display those with highest cosine similarity
+print(similarities.nlargest())
