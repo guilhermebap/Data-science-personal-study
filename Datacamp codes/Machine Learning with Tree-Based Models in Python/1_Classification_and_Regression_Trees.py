d96c328ee6bff155622fd003086871dc5e08c4d2
Guilhermebap <guilhermebarrosap@gmail.com>
2020-02-08 02:43:31 -0300

    Uploading more exercises did

--- Datacamp codes/Machine Learning with Tree-Based Models in Python/1_Classification_and_Regression_Trees.py
+++ Datacamp codes/Machine Learning with Tree-Based Models in Python/1_Classification_and_Regression_Trees.py
@@ -68,3 +68,37 @@ plot_labeled_decision_regions(X_test, y_test, clfs)
 
 
 '''
+Using entropy as a criterion
+'''
+# Import DecisionTreeClassifier from sklearn.tree
+from sklearn.tree import DecisionTreeClassifier
+
+# Instantiate dt_entropy, set 'entropy' as the information criterion
+dt_entropy = DecisionTreeClassifier(max_depth=8, criterion='entropy', random_state=1)
+
+# Fit dt_entropy to the training set
+dt_entropy.fit(X_train, y_train)
+
+
+
+'''
+Entropy vs Gini index
+'''
+# Import accuracy_score from sklearn.metrics
+from sklearn.metrics import accuracy_score
+
+# Use dt_entropy to predict test set labels
+y_pred = dt_entropy.predict(X_test)
+
+# Evaluate accuracy_entropy
+accuracy_entropy = accuracy_score(y_test, y_pred)
+
+# Print accuracy_entropy
+print('Accuracy achieved by using entropy: ', accuracy_entropy)
+
+# Print accuracy_gini
+print('Accuracy achieved by using the gini index: ', accuracy_gini)
+
+
+
+'''
