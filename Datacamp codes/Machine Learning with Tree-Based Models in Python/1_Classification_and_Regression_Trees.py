f8652fd4796bf017f0c17fa7207be0b3d4d8de4e
Guilhermebap <guilhermebarrosap@gmail.com>
2020-02-09 22:13:23 -0300

    Uploading more exercises did

--- Datacamp codes/Machine Learning with Tree-Based Models in Python/1_Classification_and_Regression_Trees.py
+++ Datacamp codes/Machine Learning with Tree-Based Models in Python/1_Classification_and_Regression_Trees.py
@@ -102,3 +102,70 @@ print('Accuracy achieved by using the gini index: ', accuracy_gini)
 
 
 '''
+Train your first regression tree
+
+In this exercise, you'll train a regression tree to predict the mpg (miles per
+gallon) consumption of cars in the auto-mpg dataset using all the six available
+features.
+'''
+# Import DecisionTreeRegressor from sklearn.tree
+from sklearn.tree import DecisionTreeRegressor
+
+# Instantiate dt
+dt = DecisionTreeRegressor(max_depth=8,
+             min_samples_leaf=0.13,
+            random_state=3)
+
+# Fit dt to the training set
+dt.fit(X_train, y_train)
+
+
+
+'''
+Evaluate the regression tree
+
+In this exercise, you will evaluate the test set performance of dt using the
+Root Mean Squared Error (RMSE) metric. The RMSE of a model measures, on average,
+how much the model's predictions differ from the actual labels. The RMSE of a
+model can be obtained by computing the square root of the model's Mean Squared
+Error (MSE).
+'''
+# Import mean_squared_error from sklearn.metrics as MSE
+from sklearn.metrics import mean_squared_error as MSE
+
+# Compute y_pred
+y_pred = dt.predict(X_test)
+
+# Compute mse_dt
+mse_dt = MSE(y_test, y_pred)
+
+# Compute rmse_dt
+rmse_dt = mse_dt**(1/2)
+
+# Print rmse_dt
+print("Test set RMSE of dt: {:.2f}".format(rmse_dt))
+
+
+
+'''
+Linear regression vs regression tree
+
+you'll compare the test set RMSE of dt to that achieved by a linear regression model.
+'''
+# Predict test set labels
+y_pred_lr = lr.predict(X_test)
+
+# Compute mse_lr
+mse_lr = MSE(y_test, y_pred_lr)
+
+# Compute rmse_lr
+rmse_lr = mse_lr**(1/2)
+
+# Print rmse_lr
+print('Linear Regression test set RMSE: {:.2f}'.format(rmse_lr))
+
+# Print rmse_dt
+print('Regression Tree test set RMSE: {:.2f}'.format(rmse_dt))
+
+'Linear Regression test set RMSE: 5.10'
+'Regression Tree test set RMSE: 4.37'
