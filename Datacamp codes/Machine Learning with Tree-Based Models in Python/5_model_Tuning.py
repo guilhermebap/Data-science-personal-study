cba31452348ace5683ab149dad18d39fb16e4e29
Guilhermebap <guilhermebarrosap@gmail.com>
2020-02-14 16:32:37 -0300

    Uploading more exercises did

--- Datacamp codes/Machine Learning with Tree-Based Models in Python/5_model_Tuning.py
+++ Datacamp codes/Machine Learning with Tree-Based Models in Python/5_model_Tuning.py
@@ -1,3 +1,56 @@
 '''
 The hyperparameters of a machine learning model are parameters that are not learned from data. They should be set prior to fitting the model to the training set. In this chapter, you'll learn how to tune the hyperparameters of a tree-based model using grid search cross validation.
 '''
+
+
+'''
+Set the tree's hyperparameter grid
+
+Your task is to tune the hyperparameters of a classification tree. Given that this dataset is imbalanced, you'll be using the ROC AUC score as a metric instead of accuracy.
+'''
+# Define params_dt
+params_dt = {
+    'max_depth': [2, 3, 4],
+    'min_samples_leaf': [0.12, 0.14, 0.16, 0.18]
+}
+
+
+
+'''
+Search for the optimal tree
+'''
+# Import GridSearchCV
+from sklearn.model_selection import GridSearchCV
+
+# Instantiate grid_dt
+grid_dt = GridSearchCV(estimator=dt,
+                       param_grid=params_dt,
+                       scoring='roc_auc',
+                       cv=5,
+                       n_jobs=-1)
+grid_dt.fit(X_train, y_train)
+
+
+
+'''
+Evaluate the optimal tree
+'''
+# Import roc_auc_score from sklearn.metrics
+from sklearn.metrics import roc_auc_score
+
+# Extract the best estimator
+best_model = grid_dt.best_estimator_
+
+# Predict the test set probabilities of the positive class
+y_pred_proba = best_model.predict_proba(X_test)[:,1]
+
+# Compute test_roc_auc
+test_roc_auc = roc_auc_score(y_test, y_pred_proba)
+
+# Print test_roc_auc
+print('Test set ROC AUC score: {:.3f}'.format(test_roc_auc))
+
+
+
+
+'''
