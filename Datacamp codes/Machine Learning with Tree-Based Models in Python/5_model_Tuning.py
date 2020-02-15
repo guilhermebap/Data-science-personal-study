3c1491c6668e10cf0b915f23e98f49454f6dfc6e
Guilhermebap <guilhermebarrosap@gmail.com>
2020-02-14 21:08:29 -0300

    Uploading more exercises did

--- Datacamp codes/Machine Learning with Tree-Based Models in Python/5_model_Tuning.py
+++ Datacamp codes/Machine Learning with Tree-Based Models in Python/5_model_Tuning.py
@@ -54,3 +54,52 @@ print('Test set ROC AUC score: {:.3f}'.format(test_roc_auc))
 
 
 '''
+Set the hyperparameter grid of RF
+'''
+# Define the dictionary 'params_rf'
+params_rf = {
+    'n_estimators': [100, 350, 500],
+    'max_features': ['log2', 'auto', 'sqrt'],
+    'min_samples_leaf': [2, 10, 30]
+}
+
+
+
+'''
+Search for the optimal forest
+
+To evaluate each model in the grid, you'll be using the negative mean squared error metric. (https://scikit-learn.org/stable/modules/model_evaluation.html)
+'''
+# Import GridSearchCV
+from sklearn.model_selection import GridSearchCV
+
+# Instantiate grid_rf
+grid_rf = GridSearchCV(estimator=rf,
+                       param_grid=params_rf,
+                       scoring='neg_mean_squared_error',
+                       cv=3,
+                       verbose=1,
+                       n_jobs=-1)
+
+grid_rf.fit(X_train, y_train)
+
+
+
+
+'''
+Evaluate the optimal forest
+'''
+# Import mean_squared_error from sklearn.metrics as MSE
+from sklearn.metrics import mean_squared_error as MSE
+
+# Extract the best estimator
+best_model = grid_rf.best_estimator_
+
+# Predict test set labels
+y_pred = best_model.predict(X_test)
+
+# Compute rmse_test
+rmse_test = MSE(y_test, y_pred) ** (1/2)
+
+# Print rmse_test
+print('Test RMSE of best model: {:.3f}'.format(rmse_test)) 
