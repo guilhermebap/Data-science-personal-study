316405aa57db4eedb59ad5ad0cf39af008ae9b04
Guilhermebap <guilhermebarrosap@gmail.com>
2020-02-18 17:07:59 -0300

    Uploading more exercises did

--- Datacamp codes/Introduction to Deep Learning in Python/3_building_deep_learning_models_with_keras.py
+++ Datacamp codes/Introduction to Deep Learning in Python/3_building_deep_learning_models_with_keras.py
@@ -78,3 +78,52 @@ model.fit(predictors, target)
 
 
 '''
+Last steps in classification models
+'''
+# Import necessary modules
+import keras
+from keras.layers import Dense
+from keras.models import Sequential
+from keras.utils import to_categorical
+
+# Convert the target to categorical: target
+target = to_categorical(df.survived)
+
+# Set up the model
+model = Sequential()
+
+# Add the first layer
+model.add(Dense(32, activation='relu', input_shape=(n_cols, )))
+
+# Add the output layer
+model.add(Dense(2, activation='softmax'))
+
+# Compile the model
+model.compile(optimizer='sgd', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Fit the model
+model.fit(predictors, target)
+
+
+
+
+'''
+Making predictions
+'''
+# Specify, compile, and fit the model
+model = Sequential()
+model.add(Dense(32, activation='relu', input_shape = (n_cols,)))
+model.add(Dense(2, activation='softmax'))
+model.compile(optimizer='sgd',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+model.fit(predictors, target)
+
+# Calculate predictions: predictions
+predictions = model.predict(pred_data)
+
+# Calculate predicted probability of survival: predicted_prob_true
+predicted_prob_true = predictions[:,1]
+
+# print predicted_prob_true
+print(predicted_prob_true)
