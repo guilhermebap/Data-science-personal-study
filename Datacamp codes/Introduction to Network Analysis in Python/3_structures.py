c2562648a04e6b4af2a085686bd7b8f1fc8f86b5
Guilhermebap <guilhermebarrosap@gmail.com>
2020-02-23 16:53:45 -0300

    Uploading more exercises did

--- Datacamp codes/Introduction to Network Analysis in Python/3_structures.py
+++ Datacamp codes/Introduction to Network Analysis in Python/3_structures.py
@@ -5,3 +5,202 @@ subgraphs, which will leverage all of the skills you acquired in Chapter 2. By
 the end of this chapter, you'll be ready to apply the concepts you've learned to
 a real-world case study.
 '''
+
+
+'''
+Identifying triangle relationships
+
+We may be interested in triangles because they're the simplest complex clique.
+
+One potential application of triangle-finding algorithms is to find out whether
+users that have similar occupations are more likely to be in a clique with one
+another.
+'''
+from itertools import combinations
+
+# Define is_in_triangle()
+def is_in_triangle(G, n):
+    """
+    Checks whether a node `n` in graph `G` is in a triangle relationship or not.
+
+    Returns a boolean.
+    """
+    in_triangle = False
+
+    # Iterate over all possible triangle relationship combinations
+    for n1, n2 in combinations(G.neighbors(n), 2):
+
+        # Check if an edge exists between n1 and n2
+        if G.has_edge(n1, n2):
+            in_triangle = True
+            break
+    return in_triangle
+
+
+
+'''
+Finding nodes involved in triangles
+
+NetworkX provides an API for counting the number of triangles that every node is
+involved in: nx.triangles(G). It returns a dictionary of nodes as the keys and
+number of triangles as the values. Your job in this exercise is to modify the
+function defined earlier to extract all of the nodes involved in a triangle
+relationship with a given node.
+'''
+from itertools import combinations
+
+# Write a function that identifies all nodes in a triangle relationship with a given node.
+def nodes_in_triangle(G, n):
+    """
+    Returns the nodes in a graph `G` that are involved in a triangle relationship with the node `n`.
+    """
+    triangle_nodes = set([n])
+
+    # Iterate over all possible triangle relationship combinations
+    for n1, n2 in combinations(G.neighbors(n), 2):
+
+        # Check if n1 and n2 have an edge between them
+        if G.has_edge(n1,n2):
+
+            # Add n1 to triangle_nodes
+            triangle_nodes.add(n1)
+
+            # Add n2 to triangle_nodes
+            triangle_nodes.add(n2)
+
+    return triangle_nodes
+
+# Write the assertion statement
+assert len(nodes_in_triangle(T, 1)) == 35
+
+
+
+
+'''
+Finding open triangles
+
+Let us now move on to finding open triangles! Recall that they form the basis of
+friend recommendation systems; if "A" knows "B" and "A" knows "C", then it's
+probable that "B" also knows "C".
+'''
+from itertools import combinations
+
+# Define node_in_open_triangle()
+def node_in_open_triangle(G, n):
+    """
+    Checks whether pairs of neighbors of node `n` in graph `G` are in an 'open triangle' relationship with node `n`.
+    """
+    in_open_triangle = False
+
+    # Iterate over all possible triangle relationship combinations
+    for n1, n2 in combinations(G.neighbors(n), 2):
+
+        # Check if n1 and n2 do NOT have an edge between them
+        if not G.has_edge(n1, n2):
+
+            in_open_triangle = True
+
+            break
+
+    return in_open_triangle
+
+# Compute the number of open triangles in T
+num_open_triangles = 0
+
+# Iterate over all the nodes in T
+for n in T.nodes():
+
+    # Check if the current node is in an open triangle
+    if node_in_open_triangle(T, n):
+
+        # Increment num_open_triangles
+        num_open_triangles += 1
+
+print(num_open_triangles)
+
+
+
+
+'''
+Finding all maximal cliques of size "n"
+
+Maximal cliques are cliques that cannot be extended by adding an adjacent edge,
+and are a useful property of the graph when finding communities.
+'''
+# Define maximal_cliques()
+def maximal_cliques(G, size):
+    """
+    Finds all maximal cliques in graph `G` that are of size `size`.
+    """
+    mcs = []
+    for clique in nx.find_cliques(G):
+        if len(clique) == size:
+            mcs.append(clique)
+    return mcs
+
+# Check that there are 33 maximal cliques of size 3 in the graph T
+assert len(maximal_cliques(T, 3)) == 33
+
+
+
+
+'''
+Subgraphs I
+'''
+nodes_of_interest = [29, 38, 42]
+
+# Define get_nodes_and_nbrs()
+def get_nodes_and_nbrs(G, nodes_of_interest):
+    """
+    Returns a subgraph of the graph `G` with only the `nodes_of_interest` and their neighbors.
+    """
+    nodes_to_draw = []
+
+    # Iterate over the nodes of interest
+    for n in nodes_of_interest:
+
+        # Append the nodes of interest to nodes_to_draw
+        nodes_to_draw.append(n)
+
+        # Iterate over all the neighbors of node n
+        for nbr in G.neighbors(n):
+
+            # Append the neighbors of n to nodes_to_draw
+            nodes_to_draw.append(nbr)
+
+    return G.subgraph(nodes_to_draw)
+
+# Extract the subgraph with the nodes of interest: T_draw
+T_draw = T.subgraph(get_nodes_and_nbrs(T, nodes_of_interest))
+
+# Draw the subgraph to the screen
+nx.draw(T_draw, with_labels=True)
+plt.show()
+
+
+
+
+'''
+Subgraphs II
+'''
+# Extract the nodes of interest: nodes
+nodes = [n for n, d in T.nodes(data=True) if d['occupation'] == 'celebrity']
+
+# Create the set of nodes: nodeset
+nodeset = set(nodes)
+
+# Iterate over nodes
+for n in nodes:
+
+    # Compute the neighbors of n: nbrs
+    nbrs = T.neighbors(n)
+
+    # Compute the union of nodeset and nbrs: nodeset
+    nodeset = nodeset.union(nbrs)
+
+# Compute the subgraph using nodeset: T_sub
+T_sub = T.subgraph(nodeset)
+
+# Draw T_sub to the screen
+nx.draw(T_sub, with_labels=True)
+plt.show()
---
+++ Datacamp codes/Introduction to Network Analysis in Python/4_bringing_it_all_together.py
@@ -0,0 +1,11 @@
+'''
+In this final chapter of the course, you'll consolidate everything you've learned
+through an in-depth case study of GitHub collaborator network data. This is a
+great example of real-world social network data, and your newly acquired skills
+will be fully tested. By the end of this chapter, you'll have developed your
+very own recommendation system to connect GitHub users who should collaborate
+together.
+'''
+
+
+'''
